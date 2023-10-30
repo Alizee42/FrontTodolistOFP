b5a703eed1769b15b69ea4b8a58658f0e38f9eb3
Alizee42 <alizee.gueye@gmail.com>
2023-10-30 20:01:35 +0100

    modification index.js page sign_in

--- sign_in/index.js
+++ sign_in/index.js
@@ -1,1 +1,23 @@
-console.log("first")
+const loginButton = document.getElementById('submit');
+
+loginButton.addEventListener('click', async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log("first")
+
+    try {
+        const response = await fetch(`http://localhost:3000/users/signin?email=${email}&password=${password}`)
+        const data = await response.json();
+
+        if (response.ok) {
+            // Authentification réussie, redirigez l'utilisateur vers la page appropriée
+            window.location.href = '/dashboard';
+        } else {
+            // Affichez un message d'erreur à l'utilisateur
+            console.error(data.message);
+        }
+    } catch (error) {
+        // Gérez les erreurs liées à la requête AJAX
+        console.error('Erreur lors de la requête AJAX:', error);
+    }
+});
