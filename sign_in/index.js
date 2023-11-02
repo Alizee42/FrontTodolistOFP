71fa6c329d296bf2451cbc510a30d2d805779d42
S7Y <sandaraly1@gmail.com>
2023-11-02 11:32:52 +0100

    condition for sign in/up done

--- sign_in/index.js
+++ sign_in/index.js
@@ -1,20 +1,26 @@
 const loginButton = document.getElementById('submit');
+const message_error = document.getElementById('message_error');
+message_error.style.color = "red";
 
 loginButton.addEventListener('click', async () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    console.log("first")
 
     try {
-        const response = await fetch(`http://localhost:3000/users/signin?email=${email}&password=${password}`)
-        const data = await response.json();
+        const rawResponse = await fetch(`http://localhost:3000/users/signin?email=${email}&password=${password}`)
+        const response = await rawResponse.json();
+        if (response.user) {
+            if (response.user) {
 
-        if (response.ok) {
-            // Authentification réussie, redirigez l'utilisateur vers la page appropriée
-            window.location.href = '../todolist_user/index.html';
-        } else {
-            // Affichez un message d'erreur à l'utilisateur
-            console.error(data.message);
+                if (response.user.role === "Admin") {
+                    window.location.href = '../todolist_admin/index.html';
+                } else {
+                    window.location.href = '../todolist_user/index.html';
+                }
+            }
+        }
+        else {
+            message_error.textContent = response.message
         }
     } catch (error) {
         // Gérez les erreurs liées à la requête AJAX
--- sign_up/index.js
+++ sign_up/index.js
@@ -1,5 +1,5 @@
 console.log("first");
-const submitButton=document.getElementById("submit");
+const submitButton = document.getElementById("submit");
 const userName = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -8,12 +8,12 @@ const message_error = document.getElementById("message-error");
 message_error.style.color = "red";
 
 
-submitButton.addEventListener("click", async ()=>{
+submitButton.addEventListener("click", async () => {
     console.log(userName.value)
     console.log(email.value)
     console.log(password.value)
     console.log("first")
-    
+
 
     let region;
     for (var i = 0; i < ville.length; i++) {
@@ -31,20 +31,26 @@ submitButton.addEventListener("click", async ()=>{
     }
 
     try {
-        const rawResponse =  await fetch('http://localhost:3000/users/signup', {
-        method: 'POST',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-        body: new URLSearchParams(data)
+        const rawResponse = await fetch('http://localhost:3000/users/signup', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: new URLSearchParams(data)
         })
 
         const response = await rawResponse.json();
-        console.log(response)
-        message_error.textContent = response.message;
+
+        if(response.user){
+            window.location.href = '../todolist_user/index.html';
+
+        } else {
+            message_error.textContent = response.message;
+        }
+
     } catch (error) {
         console.log(error)
     }
-    
+
 })
 
