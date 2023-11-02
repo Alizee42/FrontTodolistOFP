a814db5a2ba13d8ee163ce2402c2ee5be1f6c244
sankou69200 <santi@atrevise.com>
2023-11-02 10:02:54 +0100

    uptade signup

--- sign_up/index.js
+++ sign_up/index.js
@@ -1,28 +1,33 @@
 console.log("first");
 const submitButton=document.getElementById("submit");
-const userName=document.getElementsByClassName("userName");
-const email=document.getElementsByClassName("email");
-const password=document.getElementsByClassName("password");
-const ville=document.getElementsByName("ville");
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const ville = document.getElementsByName("ville");
 
 
 submitButton.addEventListener("click", async ()=>{
-    console.log(userName[0].value)
-    console.log(email[0].value)
-    console.log(password[0].value)
-    for (let radio of value) {
-        if (value.checked) {
-            console.log(value)
+    console.log(userName.value)
+    console.log(email.value)
+    console.log(password.value)
+    console.log("first")
+    
+
+    let region;
+    for (var i = 0; i < ville.length; i++) {
+        if (ville[i].checked) {
+            region = ville[i].value;
+            console.log(region)
         }
     }
-    console.log("first")
 
     const rawResponse = await fetch('http://localhost:3000/users/signup', {
         method: 'POST',
         body: JSON.stringify({
-            username:userName[0].value, 
-            email: email[0].value,
-            password: password[0].value,
+            username:username.value, 
+            email: email.value,
+            password: password.value,
+            region: region
         })
     })
 
--- todolist_admin/index.js
+++ todolist_admin/index.js
@@ -1,1 +1,9 @@
-const list = document.getElementsByClassName("list");
+const list = document.getElementById("urgent");
+
+async function getTasks(){
+    const rawResponse = await fetch('http://localhost:3000/getTasks')
+    const response = await rawResponse.json()
+    console.log(response)
+}
+
+getTasks();
